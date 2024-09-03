d0e3318a169b99717105261b965313cbf0787ffa
Alexis Garcia Saez <lexisgasa@uoc.edu>
2024-09-03 22:59:24 +0200

    Add lint-staged

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     setupFiles: ["./src/setupTests.ts"],
     environment: "jsdom",
     coverage: {
-      include: ["src/**/*.tsx"],
+      include: ["src/**/*.test.tsx"],
       exclude: ["src/main.tsx"],
     },
   },
