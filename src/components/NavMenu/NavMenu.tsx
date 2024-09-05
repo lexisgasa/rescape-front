9e3e0a0a977e84a2ed61e98d3248c79ae2cfa04e
Alexis Garcia Saez <lexisgasa@uoc.edu>
2024-09-05 18:08:37 +0200

    Create hamburger menu

--- src/components/NavMenu/NavMenu.tsx
+++ src/components/NavMenu/NavMenu.tsx
@@ -1,16 +1,36 @@
+import { RxHamburgerMenu } from "react-icons/rx";
+import { IoCloseOutline } from "react-icons/io5";
 import "./NavMenu.css";
+import { useState } from "react";
 
 const NavMenu = () => {
+  const [isHamburger, setIsHamburger] = useState(true);
+
+  const toggleMenu = (): void => {
+    setIsHamburger((currentValue): boolean => {
+      return !currentValue;
+    });
+  };
+
   return (
     <nav>
-      <ul className="navigation-links">
-        <li className="navigation-links__link--active">
-          <a href="/listado">Listado</a>
-        </li>
-        <li>
-          <a href="/crear-escape">Añadir</a>
-        </li>
-      </ul>
+      {isHamburger ? (
+        <button onClick={toggleMenu}>
+          <RxHamburgerMenu className="hamburger" />
+        </button>
+      ) : (
+        <ul className="navigation-links">
+          <li className="navigation-links__link--active">
+            <a href="/listado">Listado</a>
+          </li>
+          <li>
+            <a href="/crear-escape">Añadir</a>
+          </li>
+          <button className="navigation-links__button" onClick={toggleMenu}>
+            <IoCloseOutline />
+          </button>
+        </ul>
+      )}
     </nav>
   );
 };
