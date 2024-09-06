ae9f046abcdf8fcf4e1364f70cb5eee19d1f0aea
Alexis Garcia Saez <lexisgasa@uoc.edu>
2024-09-06 16:43:49 +0200

    Create EscapeRoomListPage test

---
+++ src/components/escapeRoom/pages/EscapeRoomListPage.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/react";
+import EscapeRoomListPage from "./EscapeRoomListPage";
+
+describe("Given the component EscapeRoomListPage", () => {
+  describe("When it is rendered", () => {
+    test("Then it should show 'Listado de escape rooms' inside a heading", () => {
+      const escapeRoomListPageTitleText = "Listado de escape rooms";
+      const escapeRoomListPageTitleTextRegex = new RegExp(
+        escapeRoomListPageTitleText,
+        "i",
+      );
+      render(<EscapeRoomListPage />);
+
+      const escapeRoomListPageTitle = screen.getByRole("heading", {
+        name: escapeRoomListPageTitleTextRegex,
+      });
+
+      expect(escapeRoomListPageTitle).toBeInTheDocument();
+    });
+  });
+});
