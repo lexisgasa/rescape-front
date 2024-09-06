1caffd2aa8347e7969b63dc7dadb663366dd859a
Alexis Garcia Saez <lexisgasa@uoc.edu>
2024-09-06 16:04:35 +0200

    Create router and configure the index route

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,12 @@
+import { Outlet } from "react-router-dom";
 import Header from "../Header/Header";
-import EscapeRoomListPage from "../escapeRoom/pages/EscapeRoomListPage";
-
 import "./App.css";
 
 const App = (): React.ReactElement => {
   return (
     <div className="container">
       <Header />
-      <EscapeRoomListPage />
+      <Outlet />
     </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./components/App/App";
+import { RouterProvider } from "react-router-dom";
+import router from "./components/router/router";
 import "@fontsource/nunito";
 import "@fontsource/nunito/700.css";
 import "./styles/styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
