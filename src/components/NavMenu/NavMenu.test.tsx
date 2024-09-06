935349780799b68bf39add34519417c58f573c08
Alexis Garcia Saez <lexisgasa@uoc.edu>
2024-09-06 13:13:21 +0200

    Create a test for the links displayed in navmenu component

--- src/components/NavMenu/NavMenu.test.tsx
+++ src/components/NavMenu/NavMenu.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import NavMenu from "./NavMenu";
+import userEvent from "@testing-library/user-event";
 
 describe("Given the NavMenu component", () => {
   describe("When it is rendered", () => {
@@ -14,6 +15,32 @@ describe("Given the NavMenu component", () => {
       });
 
       expect(NavMenuButton).toBeInTheDocument();
+    });
+  });
+
+  describe("When it receives an action and it's clicked", () => {
+    test("Then it should show the texts 'Listado' and 'Añadir' within a container", async () => {
+      const buttonAccessibleName = "Show navigation links";
+      const buttonAccesibleNameRegex = new RegExp(buttonAccessibleName, "i");
+      const listLinkText = "Listado";
+      const createLinkTest = "Añadir";
+
+      render(<NavMenu />);
+
+      const NavMenuButton = screen.getByRole("button", {
+        name: buttonAccesibleNameRegex,
+      });
+
+      const user = userEvent.setup();
+      await user.click(NavMenuButton);
+
+      screen.debug();
+
+      const listLink = screen.getByRole("link", { name: listLinkText });
+      const createLink = screen.getByRole("link", { name: createLinkTest });
+
+      expect(listLink).toBeInTheDocument();
+      expect(createLink).toBeInTheDocument();
     });
   });
 });
