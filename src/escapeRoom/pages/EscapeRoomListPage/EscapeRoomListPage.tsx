696100999f36deafc84c5ee93dbcb896564a0679
Alexis Garcia Saez <lexisgasa@uoc.edu>
2024-09-13 10:31:27 +0200

    Style and structure escape room card markup

--- src/escapeRoom/pages/EscapeRoomListPage/EscapeRoomListPage.tsx
+++ src/escapeRoom/pages/EscapeRoomListPage/EscapeRoomListPage.tsx
@@ -1,4 +1,6 @@
 import { EscapeRoomClient } from "../../api/EscapeRoomClient";
+import { FaTrashCan } from "react-icons/fa6";
+import "./EscapeRoomListPage.css";
 
 const EscapeRoomListPage = (): React.ReactElement => {
   const escapeRoomClient = new EscapeRoomClient();
@@ -7,7 +9,18 @@ const EscapeRoomListPage = (): React.ReactElement => {
 
   return (
     <main>
-      <h1 className="main-title">Listado de escape rooms</h1>;
+      <h1 className="main-title">Listado de escape rooms</h1>
+      <article className="card">
+        <img src="" alt="" height={125} width={125} />
+        <div>
+          <span></span>
+          <h2></h2>
+          <p></p>
+        </div>
+        <button className="card__button">
+          <FaTrashCan className="garbage-can" />
+        </button>
+      </article>
     </main>
   );
 };
