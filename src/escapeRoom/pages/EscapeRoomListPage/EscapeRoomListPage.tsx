1e1422b452cb6084d15a61dc9bd0c3c33a2a055a
Alexis Garcia Saez <lexisgasa@uoc.edu>
2024-09-15 10:12:31 +0200

    Create EscapeRoomCard component and a Zustand store

--- src/escapeRoom/pages/EscapeRoomListPage/EscapeRoomListPage.tsx
+++ src/escapeRoom/pages/EscapeRoomListPage/EscapeRoomListPage.tsx
@@ -1,26 +1,40 @@
 import { EscapeRoomClient } from "../../api/EscapeRoomClient";
-import { FaTrashCan } from "react-icons/fa6";
+import useAppStore from "../../../store/useAppStore";
+import { useEffect, useMemo } from "react";
+import EscapeRoomCard from "../../components/EscapeRoomCard";
 import "./EscapeRoomListPage.css";
 
 const EscapeRoomListPage = (): React.ReactElement => {
-  const escapeRoomClient = new EscapeRoomClient();
+  const { escapeRooms, loadEscapeRoom, isLoading, setIsLoading } =
+    useAppStore();
 
-  escapeRoomClient.getEscapeRooms();
+  const escapeRoomClient = useMemo(() => new EscapeRoomClient(), []);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    const fetchEscapeRooms = async () => {
+      try {
+        const apiEscapeRooms = await escapeRoomClient.getEscapeRooms();
+        loadEscapeRoom(apiEscapeRooms);
+        setIsLoading(false);
+      } catch (error) {
+        setIsLoading(false);
+        throw new Error(`No se han podido obtener los datos ${error}`);
+      }
+    };
+
+    fetchEscapeRooms();
+  }, [escapeRoomClient, loadEscapeRoom, setIsLoading]);
 
   return (
     <main>
       <h1 className="main-title">Listado de escape rooms</h1>
-      <article className="card">
-        <img src="" alt="" height={125} width={125} />
-        <div>
-          <span></span>
-          <h2></h2>
-          <p></p>
-        </div>
-        <button className="card__button">
-          <FaTrashCan className="garbage-can" />
-        </button>
-      </article>
+      {isLoading ? (
+        <h2>Cargando contenido..</h2>
+      ) : (
+        escapeRooms.length > 0 && <EscapeRoomCard escapeRoom={escapeRooms[0]} />
+      )}
     </main>
   );
 };
