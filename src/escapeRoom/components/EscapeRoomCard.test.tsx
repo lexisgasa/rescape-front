8bc77d55f324b85d8a030e957f8ab6b5d4753e7a
Alexis Garcia Saez <lexisgasa@uoc.edu>
2024-09-15 11:04:55 +0200

    Create test for EscapeRoomCard component

---
+++ src/escapeRoom/components/EscapeRoomCard.test.tsx
@@ -0,0 +1,31 @@
+import { render, screen } from "@testing-library/react";
+import EscapeRoomCard from "./EscapeRoomCard";
+import { EscapeRoom } from "../types";
+
+describe("Given the component EscapeRoomCard", () => {
+  describe("When it receives an escape room with the title 'Outline'", () => {
+    test("Then it should the escape room's title 'Outline' inside a heading", () => {
+      const escapeRoom: EscapeRoom = {
+        id: "",
+        title: "Outline",
+        alternativeText: "",
+        smallImage: "",
+        bigImage: "",
+        date: new Date(),
+        description: "",
+        place: "",
+        rating: 5,
+      };
+      const escapeRoomHeadingText = "Outline";
+      const escapeRoomHeadingTextRegex = new RegExp(escapeRoomHeadingText, "i");
+
+      render(<EscapeRoomCard escapeRoom={escapeRoom} />);
+
+      const escapeRoomHeading = screen.getByRole("heading", {
+        name: escapeRoomHeadingTextRegex,
+      });
+
+      expect(escapeRoomHeading).toBeInTheDocument();
+    });
+  });
+});
