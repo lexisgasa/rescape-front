ad7c9b5c83b1413df4ce2587bfc41a750a572f0d
Alexis Garcia Saez <lexisgasa@uoc.edu>
2024-09-15 12:56:52 +0200

    Create EscapeRoomList component

--- src/escapeRoom/components/EscapeRoomCard/EscapeRoomCard.tsx
+++ src/escapeRoom/components/EscapeRoomCard/EscapeRoomCard.tsx
@@ -1,5 +1,6 @@
 import { FaTrashCan } from "react-icons/fa6";
 import { EscapeRoom } from "../../types";
+import "./EscapeRoomCard.css";
 
 interface EscapeRoomProps {
   escapeRoom: EscapeRoom;
@@ -8,13 +9,19 @@ interface EscapeRoomProps {
 const EscapeRoomCard = ({
   escapeRoom: { title, place, smallImage, alternativeText, description },
 }: EscapeRoomProps): React.ReactElement => {
+  const maximumCharacters = 100;
+
   return (
     <article className="card">
       <img src={smallImage} alt={alternativeText} height={125} width={125} />
       <div>
         <span>{place}</span>
         <h2>{title}</h2>
-        <p>{description}</p>
+        <p>
+          {description && description.length > maximumCharacters
+            ? `${description.slice(0, 90)}...`
+            : description}
+        </p>
       </div>
       <button className="card__button">
         <FaTrashCan className="garbage-can" />
